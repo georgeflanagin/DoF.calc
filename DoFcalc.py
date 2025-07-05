d28991e9ab9628fc0ef83eeb02bfe1e23f88cb6f
georgeflanagin <milesdavis@billevans>
2025-07-05 16:31:45 -0400

    Original version.

---
+++ DoFcalc.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+import typing
+from   typing import *
+
+###
+# Standard imports, starting with os and sys
+###
+min_py = (3, 11)
+import os
+import sys
+if sys.version_info < min_py:
+    print(f"This program requires Python {min_py[0]}.{min_py[1]}, or higher.")
+    sys.exit(os.EX_SOFTWARE)
+
+###
+# Other standard distro imports
+###
+import argparse
+from   collections.abc import *
+import contextlib
+import getpass
+import logging
+from   logging import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+
+###
+# Installed libraries like numpy, pandas, paramiko
+###
+
+###
+# From hpclib
+###
+import linuxutils
+from   urdecorators import trap
+from   urlogger import URLogger
+
+###
+# imports and objects that were written for this project.
+###
+
+###
+# Global objects
+###
+logger = None
+f_stops = (1.0, 1.4, 1.8, 2.0, 2.4, 2.8, 3.5, 4, 5.6, 6.3, 7.1, 8, 11, 16, 22)
+
+
+###
+# Credits
+###
+__author__ = 'George Flanagin'
+__copyright__ = 'Copyright 2025'
+__credits__ = None
+__version__ = 0.1
+__maintainer__ = 'George Flanagin'
+__email__ = 'gflanagin@richmond.edu'
+__status__ = 'in progress'
+__license__ = 'MIT'
+
+
+@trap
+def DoFcalc_main(myargs:argparse.Namespace) -> int:
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+
+    here       = os.getcwd()
+    progname   = os.path.basename(__file__)[:-3]
+    configfile = f"{here}/{progname}.toml"
+    logfile    = f"{here}/{progname}.log"
+    lockfile   = f"{here}/{progname}.lock"
+    
+    parser = argparse.ArgumentParser(prog="DoFcalc", 
+        description="""
+What DoFcalc does, DoFcalc does best. 
+
+You supply the basic lens specs. Example: 105mm f/2 as 
+--max 2 --len 105. If you know the minimum focus distance, you 
+can supply it as --min 2.0 for 2.0 meters. The default is 1 meter.
+
+You can also supply the circle of confusion in microns, but the
+default of --circle 15.0 is suitable for most modern cameras.
+
+The result is a chart-able csv file or a pandas data frame if
+you have pandas installed on this computer.
+
+""")
+
+
+    # Standard arguments.
+    parser.add_argument('--log-level', type=int, default=INFO,
+        choices=(CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET),
+        help=f"Logging level, defaults to {logging.INFO}")
+
+    parser.add_argument('-o', '--output', type=str, default="",
+        help="Output file name")
+    
+    parser.add_argument('-z', '--zap', action='store_true', 
+        help="Remove old log file and create a new one.")
+
+
+    # Arguments for this program.
+    parser.add_argument('--len', type=int, required=True,
+        help="Focal length of the lens in mm.")
+
+    parser.add_argument('--max', type=float, required=True,
+        help="f-stop when the lens is wide open.")
+
+    parser.add_argument('--min', type=float, default=1.0, 
+        help="Minimum focus distance. Defaults to 1 meter.")
+
+    parser.add_argument('--format', type=str, default='csv',
+        choices=('csv', 'eqs', 'pandas'),
+         help="Output format: csv, eqs, or pandas.")
+
+    parser.add_argument('-c', '--circle', type=float, default=15.0,
+        choices=range(10,21), 
+        help="Circle of confusion. Default: 15 microns (0.015mm)")
+
+    myargs = parser.parse_args()
+    if myargs.zap:
+        try:
+            unlink(logfile)
+        except:
+            pass
+
+
+    logger = URLogger(logfile=logfile, level=myargs.loglevel)
+
+    try:
+        outfile = sys.stdout if not myargs.output else open(myargs.output, 'w')
+        with contextlib.redirect_stdout(outfile):
+            sys.exit(globals()[f"{progname}_main"](myargs))
+
+    except Exception as e:
+        print(f"Escaped or re-raised exception: {e}")
+
