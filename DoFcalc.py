6647102370b366b15f7648e1cece6a0931a021eb
georgeflanagin <milesdavis@billevans>
2025-07-07 16:38:57 -0400

    Updated.

--- DoFcalc.py
+++ DoFcalc.py
@@ -21,10 +21,13 @@ import contextlib
 import getpass
 import logging
 from   logging import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+import math
 
 ###
 # Installed libraries like numpy, pandas, paramiko
 ###
+import numpy
+import pandas
 
 ###
 # From hpclib
@@ -41,8 +44,10 @@ from   urlogger import URLogger
 # Global objects
 ###
 logger = None
-f_stops = (1.0, 1.4, 1.8, 2.0, 2.4, 2.8, 3.5, 4, 5.6, 6.3, 7.1, 8, 11, 16, 22)
-
+f_stops = ( 1.0, 1.4, 1.8, 2.0, 
+            2.4, 2.8, 3.5, 4.0, 
+            5.6, 6.3, 7.1, 8.0, 
+            11.0, 16.0, 22.0)
 
 ###
 # Credits
@@ -58,7 +63,46 @@ __license__ = 'MIT'
 
 
 @trap
+def hyperfocal(focal_len:float, f:float, circle:float) -> float:
+    return focal_len * focal_len / f / circle
+
+
+def intervals(hyperfocal:float, min_dist:float, n:int) -> tuple:
+    multiplier = math.pow(hyperfocal/min_dist, 1/float(n))
+    return [ min_dist * math.pow(multiplier, i) for i in range(0, n+1)]
+
+@trap
+def normalize_units(myargs:argparse.Namespace) -> argparse.Namespace:
+    myargs.min_dist=myargs.min_dist*1000
+    myargs.min_dist = max(myargs.min_dist, 2*myargs.length)
+    myargs.circle = myargs.circle/1000
+
+    return myargs
+
+
+@trap
+def thin_lens(length:float,
+        max_aperture:float,
+        circle:float,
+        min_dist:float,
+        hyperfocal:float
+        ) -> pandas.DataFrame:
+    """
+    Return a data frame containing near and far focus limits 
+    across the parameters given.
+    """
+    global f_stops
+    return 
+
+
+@trap
 def DoFcalc_main(myargs:argparse.Namespace) -> int:
+    """
+    Step 1 is the normalization of the different units of measure.
+        We are going to put everything in millimeters.
+    """
+    myargs = normalize_units(myargs)
+
     return os.EX_OK
 
 
@@ -100,13 +144,13 @@ you have pandas installed on this computer.
 
 
     # Arguments for this program.
-    parser.add_argument('--len', type=int, required=True,
+    parser.add_argument('--len', '--length', type=int, required=True,
         help="Focal length of the lens in mm.")
 
-    parser.add_argument('--max', type=float, required=True,
+    parser.add_argument('--max_aperture', type=float, required=True,
         help="f-stop when the lens is wide open.")
 
-    parser.add_argument('--min', type=float, default=1.0, 
+    parser.add_argument('--min_dist', type=float, default=1.0, 
         help="Minimum focus distance. Defaults to 1 meter.")
 
     parser.add_argument('--format', type=str, default='csv',
@@ -114,7 +158,6 @@ you have pandas installed on this computer.
          help="Output format: csv, eqs, or pandas.")
 
     parser.add_argument('-c', '--circle', type=float, default=15.0,
-        choices=range(10,21), 
         help="Circle of confusion. Default: 15 microns (0.015mm)")
 
     myargs = parser.parse_args()
